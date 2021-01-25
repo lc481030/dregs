849b9e4f6c5359a96f8a4ac70228517c1cd62e72
lichao <lc481030@qq.com>
2021-01-25 15:55:57 +0800

    1

--- src/main/java/com/dregs/project/system/pay/domain/TProjectPay.java
+++ src/main/java/com/dregs/project/system/pay/domain/TProjectPay.java
@@ -2,6 +2,7 @@ package com.dregs.project.system.pay.domain;
 
 import com.dregs.framework.aspectj.lang.annotation.Excel;
 import com.dregs.framework.web.domain.BaseEntity;
+import lombok.Data;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 
@@ -11,6 +12,7 @@ import org.apache.commons.lang.builder.ToStringStyle;
  * @author dregs
  * @date 2021-01-20
  */
+@Data
 public class TProjectPay extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
@@ -64,166 +66,5 @@ public class TProjectPay extends BaseEntity
     private Long projectId;
     private Long slagyardId;
 
-    public Long getCarId() {
-        return carId;
-    }
-
-    public void setCarId(Long carId) {
-        this.carId = carId;
-    }
-
-    public Long getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(Long projectId) {
-        this.projectId = projectId;
-    }
-
-    public Long getSlagyardId() {
-        return slagyardId;
-    }
-
-    public void setSlagyardId(Long slagyardId) {
-        this.slagyardId = slagyardId;
-    }
-
-    public String getBeginTime() {
-        return beginTime;
-    }
-
-    public void setBeginTime(String beginTime) {
-        this.beginTime = beginTime;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
-
-    public String getTypeName() {
-        return typeName;
-    }
-
-    public void setTypeName(String typeName) {
-        this.typeName = typeName;
-    }
-
-    public String getRelationName() {
-        return relationName;
-    }
-
-    public void setRelationName(String relationName) {
-        this.relationName = relationName;
-    }
-
-    public String getPayTypeName() {
-        return payTypeName;
-    }
-
-    public void setPayTypeName(String payTypeName) {
-        this.payTypeName = payTypeName;
-    }
-
-    public void setId(Long id)
-    {
-        this.id = id;
-    }
-
-    public Long getId()
-    {
-        return id;
-    }
-    public void setAddUserId(Long addUserId)
-    {
-        this.addUserId = addUserId;
-    }
-
-    public Long getAddUserId()
-    {
-        return addUserId;
-    }
-    public void setAddName(String addName)
-    {
-        this.addName = addName;
-    }
-
-    public String getAddName()
-    {
-        return addName;
-    }
-    public void setUpdUserId(Long updUserId)
-    {
-        this.updUserId = updUserId;
-    }
-
-    public Long getUpdUserId()
-    {
-        return updUserId;
-    }
-    public void setUdpName(String udpName)
-    {
-        this.udpName = udpName;
-    }
-
-    public String getUdpName()
-    {
-        return udpName;
-    }
-    public void setPayType(Long payType)
-    {
-        this.payType = payType;
-    }
-
-    public Long getPayType()
-    {
-        return payType;
-    }
-    public void setMoney(String money)
-    {
-        this.money = money;
-    }
-
-    public String getMoney()
-    {
-        return money;
-    }
-    public void setRelationId(Long relationId)
-    {
-        this.relationId = relationId;
-    }
-
-    public Long getRelationId()
-    {
-        return relationId;
-    }
-    public void setType(String type)
-    {
-        this.type = type;
-    }
-
-    public String getType()
-    {
-        return type;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
-            .append("id", getId())
-            .append("createTime", getCreateTime())
-            .append("updateTime", getUpdateTime())
-            .append("addUserId", getAddUserId())
-            .append("addName", getAddName())
-            .append("updUserId", getUpdUserId())
-            .append("udpName", getUdpName())
-            .append("payType", getPayType())
-            .append("money", getMoney())
-            .append("relationId", getRelationId())
-            .append("type", getType())
-            .toString();
-    }
+    private String payObjId;
 }
